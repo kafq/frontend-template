f1a5c4e6b4fc06e38abed4cd42546f581a6dfd63
Denis Bakhmutov <dbak@FL430.local>
2018-11-12 11:18:17 +0200

    Use Button in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
 import React, { Component } from 'react';
+import { Button } from './common/components';
 
 class App extends Component {
+  onPress = () => alert('Hello, too');
+
   render() {
-    return <div className="App">Hello World</div>;
+    return (
+      <div className="App">
+        Hello <Button title="World" onClick={this.onPress} />{' '}
+      </div>
+    );
   }
 }
 
