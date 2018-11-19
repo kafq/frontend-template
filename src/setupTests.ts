be29291254787fd380f280b429c2c632ad1bbf27
Denis Bakhmutov <dbak@client-1-83.guest.futurice.org>
2018-11-19 11:34:23 +0200

    Add comments to setupTests

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,7 +1,10 @@
+// adds DOM-specific matchers, e.g. toBeInTheDocument, toBeDisabled
 import 'jest-dom/extend-expect';
+// unmounts react elements after each test
 import 'react-testing-library/cleanup-after-each';
 import * as emotion from 'emotion';
 
+// allows to see expected/actual css right inside of the test
 import { createSerializer } from 'jest-emotion';
 
 expect.addSnapshotSerializer(createSerializer(emotion));
