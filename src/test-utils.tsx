7c7a2f359013cc21bc14eb33c3c7a63dc04c6fce
Denis Bakhmutov <dbak@client-1-83.guest.futurice.org>
2018-11-19 10:40:04 +0200

    Setup test render to provide custom redux context

--- src/test-utils.tsx
+++ src/test-utils.tsx
@@ -2,17 +2,31 @@ import React from 'react';
 import { render as rtlRender } from 'react-testing-library';
 import { createMemoryHistory } from 'history';
 import { Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { store } from './store';
 
-function renderWithHistory(
+// Override of react-testing-library allows to create routing and redux context for any test
+
+function render(
   ui: any,
   {
-    route = '/', history = createMemoryHistory({ initialEntries: [route] }), ...options
+    route = '/',
+    history = createMemoryHistory({ initialEntries: [route] }),
+    initialState = {},
+    ...renderOptions
   }: any = {},
 ) {
   return {
-    ...rtlRender(<Router history={history}>{ui}</Router>, options),
+    ...rtlRender(
+      <Provider store={store(initialState)}>
+        <Router history={history}>
+          {ui}
+        </Router>
+      </Provider>,
+      renderOptions,
+    ),
     history,
   };
 }
 
-export { renderWithHistory }
+export { render };
